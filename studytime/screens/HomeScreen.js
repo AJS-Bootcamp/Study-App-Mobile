1108f053e4eae30f77f4dae4ca5e63ad9f4df8f3
Sylvia Barcena <barcena.sylvia8@gmail.com>
2023-11-03 13:39:35 -0700

    speed dial

--- studytime/screens/HomeScreen.js
+++ studytime/screens/HomeScreen.js
@@ -1,11 +1,54 @@
-import { View, Text } from "react-native";
+import { Text, ScrollView } from "react-native";
+
+import { useState } from "react";
+import { SpeedDial } from "react-native-elements";
+import { Linking } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const HomeScreen = () => {
-    return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-    );
+  const [open, setOpen] = useState(false);
+  return (
+    <ScrollView>
+      <Text
+        style={{
+          color: "#5637DD",
+
+          fontSize: 40,
+        }}
+      >
+        HomeScreen
+      </Text>
+      <SpeedDial
+        isOpen={open}
+        icon={{ name: "star", color: "#fff" }}
+        openIcon={{ name: "close", color: "#fff" }}
+        onOpen={() => setOpen(!open)}
+        onClose={() => setOpen(!open)}
+        buttonStyle={{ backgroundColor: "#5637DD" }}
+      >
+        <SpeedDial.Action
+          icon={<Icon name="facebook" size={20} color="white" />}
+          title="Facebook"
+          onPress={() => Linking.openURL("https://www.facebook.com/")}
+          buttonStyle={{ backgroundColor: "#4267B2" }}
+        ></SpeedDial.Action>
+
+        <SpeedDial.Action
+          icon={<Icon name="instagram" size={20} color="white" />}
+          title="Instagram"
+          onPress={() => Linking.openURL("https://www.instagram.com/")}
+          buttonStyle={{ backgroundColor: "#E1306C" }}
+        ></SpeedDial.Action>
+
+        <SpeedDial.Action
+          icon={<Icon name="github" size={20} color="white" />}
+          title="GitHub"
+          onPress={() => Linking.openURL("https://www.github.com/")}
+          buttonStyle={{ backgroundColor: "#333" }}
+        ></SpeedDial.Action>
+      </SpeedDial>
+    </ScrollView>
+  );
 };
 
 export default HomeScreen;
--- studytime/screens/MainComponent.js
+++ studytime/screens/MainComponent.js
@@ -7,44 +7,44 @@ import StudyScreen from "./StudyScreen";
 import ContactScreen from "./ContactScreen";
 
 const FirstRoute = () => (
-    <HomeScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
+  <HomeScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
 );
 
 const SecondRoute = () => (
-    <StudyScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
+  <StudyScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
 );
 const ThirdRoute = () => (
-    <AboutScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
+  <AboutScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
 );
 
 const FourthRoute = () => (
-    <ContactScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
+  <ContactScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
 );
 
 const renderScene = SceneMap({
-    first: FirstRoute,
-    second: SecondRoute,
-    third: ThirdRoute,
-    fourth: FourthRoute,
+  first: FirstRoute,
+  second: SecondRoute,
+  third: ThirdRoute,
+  fourth: FourthRoute,
 });
 
 export default function TabViewMain() {
-    const layout = useWindowDimensions();
+  const layout = useWindowDimensions();
 
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-        { key: "first", title: "Home" },
-        { key: "second", title: "Study" },
-        { key: "third", title: "About" },
-        { key: "fourth", title: "Contact" },
-    ]);
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "Home" },
+    { key: "second", title: "Study" },
+    { key: "third", title: "About" },
+    { key: "fourth", title: "Contact" },
+  ]);
 
-    return (
-        <TabView
-            navigationState={{ index, routes }}
-            renderScene={renderScene}
-            onIndexChange={setIndex}
-            initialLayout={{ width: layout.width }}
-        />
-    );
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
 }
