5b8fb150abd02553e5d1b50e6d7a45a3ce672092
Sylvia Barcena <barcena.sylvia8@gmail.com>
2023-11-06 20:42:10 -0800

    platform.os, main component, add header, add logo

--- studytime/screens/MainComponent.js
+++ studytime/screens/MainComponent.js
@@ -1,10 +1,14 @@
+import { Platform } from "react-native";
+import Constants from "expo-constants";
 import * as React from "react";
-import { View, useWindowDimensions } from "react-native";
+import { View, useWindowDimensions, Text, StyleSheet } from "react-native";
 import { TabView, SceneMap } from "react-native-tab-view";
 import HomeScreen from "./HomeScreen";
 import AboutScreen from "./AboutScreen";
 import StudyScreen from "./StudyScreen";
 import ContactScreen from "./ContactScreen";
+import logo from "../assets/img/lightAJS.png";
+import { Image } from "react-native";
 
 const FirstRoute = () => (
   <HomeScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
@@ -39,12 +43,42 @@ export default function TabViewMain() {
     { key: "fourth", title: "Contact" },
   ]);
 
+  // ...
+
   return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: layout.width }}
-    />
+    <View
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+      }}
+    >
+      <View style={styles.header}>
+        <Image source={logo} style={{ width: 50, height: 50 }} />
+        <Text style={styles.headerText}>Your Study App</Text>
+      </View>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 50,
+    backgroundColor: "#f5f5f5",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
