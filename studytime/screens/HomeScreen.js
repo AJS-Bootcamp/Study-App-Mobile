ab277b6eef2d975172ee4c6f265f10dca8c99854
Sylvia Barcena <barcena.sylvia8@gmail.com>
2023-11-09 10:49:46 -0800

    yellow progress bar

--- studytime/screens/HomeScreen.js
+++ studytime/screens/HomeScreen.js
@@ -7,11 +7,11 @@ import * as Progress from "react-native-progress";
 
 import React from "react";
 import {
-    RefreshControl,
-    Text,
-    ScrollView,
-    StyleSheet,
-    View,
+  RefreshControl,
+  Text,
+  ScrollView,
+  StyleSheet,
+  View,
 } from "react-native";
 
 //Speedial with social media
@@ -21,152 +21,146 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 import { LinearGradient } from "expo-linear-gradient";
 
+// "#008080" #09B4B7 #06466B #E5E8E7 #B3B5B7 #010114 #FFFF00
+
 const wait = (timeout) => {
-    return new Promise((resolve) => setTimeout(resolve, timeout));
+  return new Promise((resolve) => setTimeout(resolve, timeout));
 };
 
 const HomeScreen = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    //colors={["#446879", "#0e122c"]}
+  const [items, setItems] = useState([]);
+  const [completionPercentage, setCompletionPercentage] = useState(0);
 
-    // "#008080" #09B4B7 #06466B #E5E8E7 #B3B5B7 #010114
+  const [refreshing, setRefreshing] = useState(false);
 
-    const [items, setItems] = useState([]);
-    const [completionPercentage, setCompletionPercentage] = useState(0);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
-    const [refreshing, setRefreshing] = useState(false);
+  function handleAddItem(item) {
+    const newItem = { id: uuid(), task: item, complete: false };
+    setItems((items) => [...items, newItem]);
+  }
 
-    const onRefresh = useCallback(() => {
-        setRefreshing(true);
-        wait(2000).then(() => setRefreshing(false));
-    }, []);
+  function handleRemoveItem(itemId) {
+    setItems(items.filter((item) => item.id !== itemId));
+  }
 
-    function handleAddItem(item) {
-        const newItem = { id: uuid(), task: item, complete: false };
-        setItems((items) => [...items, newItem]);
+  function calculateCompletionPercentage(items) {
+    const totalTasks = items.length;
+    if (totalTasks === 0) {
+      return 0;
     }
+    const completedTasks = items.filter((item) => item.complete).length;
+    const percentage = (completedTasks / totalTasks) * 100;
+    return percentage;
+  }
 
-    function handleRemoveItem(itemId) {
-        setItems(items.filter((item) => item.id !== itemId));
-    }
-
-    function calculateCompletionPercentage(items) {
-        const totalTasks = items.length;
-        if (totalTasks === 0) {
-            return 0;
-        }
-        const completedTasks = items.filter((item) => item.complete).length;
-        const percentage = (completedTasks / totalTasks) * 100;
-        return percentage;
-    }
-
-    const completeTask = (itemId) => {
-        const updatedItems = items.map((item) =>
-            item.id === itemId ? { ...item, complete: !item.complete } : item
-        );
-        // Update the items state with the updated completion status
-        setItems(updatedItems);
-    };
-
-    useEffect(() => {
-        const percentage = calculateCompletionPercentage(items);
-        setCompletionPercentage(+percentage.toFixed(2));
-    }, [items]); // Recalculate percentage whenever items change
-
-    return (
-        <LinearGradient
-            colors={["#06466B", "#010114"]}
-            style={styles.container}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-        >
-            <ScrollView
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                    />
-                }
-            >
-                <View style={{ flex: 4 }}>
-                    <Text
-                        style={{
-                            textAlign: "center",
-                            fontSize: 30,
-                            fontWeight: "600",
-                            marginVertical: 10,
-                            color: "white",
-                        }}
-                    >
-                        Study Tasks
-                    </Text>
-                    <TaskForm addItem={handleAddItem} />
-                    <Text
-                        style={{
-                            textAlign: "center",
-                            fontSize: 20,
-                            fontWeight: "bold",
-                            color: "white",
-                        }}
-                    >
-                        {completionPercentage}%
-                    </Text>
-                    <Progress.Bar
-                        progress={completionPercentage / 100}
-                        width={350}
-                        height={15}
-                        style={{ alignSelf: "center", marginBottom: 25 }}
-                    />
-                    <TaskList
-                        items={items}
-                        removeItem={handleRemoveItem}
-                        completeTask={completeTask}
-                    />
-                </View>
-            </ScrollView>
-            <SpeedDial
-                isOpen={open}
-                icon={{ name: "star", color: "#fff" }}
-                openIcon={{ name: "close", color: "#fff" }}
-                onOpen={() => setOpen(!open)}
-                onClose={() => setOpen(!open)}
-                buttonStyle={{ backgroundColor: "#0ABAB5" }}
-                style={{ flex: 1 }}
-            >
-                <SpeedDial.Action
-                    icon={<Icon name="facebook" size={20} color="white" />}
-                    title="Facebook"
-                    onPress={() => Linking.openURL("https://www.facebook.com/")}
-                    buttonStyle={{ backgroundColor: "#4267B2" }}
-                ></SpeedDial.Action>
-
-                <SpeedDial.Action
-                    icon={<Icon name="instagram" size={20} color="white" />}
-                    title="Instagram"
-                    onPress={() =>
-                        Linking.openURL("https://www.instagram.com/")
-                    }
-                    buttonStyle={{ backgroundColor: "#E1306C" }}
-                ></SpeedDial.Action>
-
-                <SpeedDial.Action
-                    icon={<Icon name="github" size={20} color="white" />}
-                    title="GitHub"
-                    onPress={() => Linking.openURL("https://www.github.com/")}
-                    buttonStyle={{ backgroundColor: "#333" }}
-                ></SpeedDial.Action>
-            </SpeedDial>
-        </LinearGradient>
+  const completeTask = (itemId) => {
+    const updatedItems = items.map((item) =>
+      item.id === itemId ? { ...item, complete: !item.complete } : item
     );
+    // Update the items state with the updated completion status
+    setItems(updatedItems);
+  };
+
+  useEffect(() => {
+    const percentage = calculateCompletionPercentage(items);
+    setCompletionPercentage(+percentage.toFixed(2));
+  }, [items]); // Recalculate percentage whenever items change
+
+  return (
+    <LinearGradient
+      colors={["#06466B", "#010114"]}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <View style={{ flex: 4 }}>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 30,
+              fontWeight: "600",
+              marginVertical: 10,
+              color: "white",
+            }}
+          >
+            Study Tasks
+          </Text>
+          <TaskForm addItem={handleAddItem} />
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 20,
+              fontWeight: "bold",
+              color: "white",
+            }}
+          >
+            {completionPercentage}%
+          </Text>
+          <Progress.Bar
+            progress={completionPercentage / 100}
+            width={350}
+            height={15}
+            style={{ alignSelf: "center", marginBottom: 25 }}
+            color="#FDE74C"
+          />
+          <TaskList
+            items={items}
+            removeItem={handleRemoveItem}
+            completeTask={completeTask}
+          />
+        </View>
+      </ScrollView>
+      <SpeedDial
+        isOpen={open}
+        icon={{ name: "star", color: "#fff" }}
+        openIcon={{ name: "close", color: "#fff" }}
+        onOpen={() => setOpen(!open)}
+        onClose={() => setOpen(!open)}
+        buttonStyle={{ backgroundColor: "#0ABAB5" }}
+        style={{ flex: 1 }}
+      >
+        <SpeedDial.Action
+          icon={<Icon name="facebook" size={20} color="white" />}
+          title="Facebook"
+          onPress={() => Linking.openURL("https://www.facebook.com/")}
+          buttonStyle={{ backgroundColor: "#4267B2" }}
+        ></SpeedDial.Action>
+
+        <SpeedDial.Action
+          icon={<Icon name="instagram" size={20} color="white" />}
+          title="Instagram"
+          onPress={() => Linking.openURL("https://www.instagram.com/")}
+          buttonStyle={{ backgroundColor: "#E1306C" }}
+        ></SpeedDial.Action>
+
+        <SpeedDial.Action
+          icon={<Icon name="github" size={20} color="white" />}
+          title="GitHub"
+          onPress={() => Linking.openURL("https://www.github.com/")}
+          buttonStyle={{ backgroundColor: "#333" }}
+        ></SpeedDial.Action>
+      </SpeedDial>
+    </LinearGradient>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default HomeScreen;
--- studytime/screens/MainComponent.js
+++ studytime/screens/MainComponent.js
@@ -2,12 +2,12 @@ import Constants from "expo-constants";
 import * as React from "react";
 
 import {
-    View,
-    useWindowDimensions,
-    Text,
-    StyleSheet,
-    Image,
-    Platform,
+  View,
+  useWindowDimensions,
+  Text,
+  StyleSheet,
+  Image,
+  Platform,
 } from "react-native";
 import { Icon } from "react-native-elements";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
@@ -17,96 +17,97 @@ import StudyScreen from "./StudyScreen";
 import ContactScreen from "./ContactScreen";
 import logo from "../assets/img/lightAJS.png";
 
+// "#008080" #09B4B7 #06466B #E5E8E7 #B3B5B7 #010114 #FFFF00
+
 const FirstRoute = () => (
-    <HomeScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
+  <HomeScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
 );
 
 const SecondRoute = () => (
-    <StudyScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
+  <StudyScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
 );
 const ThirdRoute = () => (
-    <AboutScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
+  <AboutScreen style={{ flex: 1, backgroundColor: "#ff4081" }} />
 );
 
 const FourthRoute = () => (
-    <ContactScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
+  <ContactScreen style={{ flex: 1, backgroundColor: "#673ab7" }} />
 );
 
 const renderScene = SceneMap({
-    first: FirstRoute,
-    second: SecondRoute,
-    third: ThirdRoute,
-    fourth: FourthRoute,
+  first: FirstRoute,
+  second: SecondRoute,
+  third: ThirdRoute,
+  fourth: FourthRoute,
 });
 
 export default function TabViewMain() {
-    const layout = useWindowDimensions();
+  const layout = useWindowDimensions();
 
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-        { key: "first", title: "Home" },
-        { key: "second", title: "Study" },
-        { key: "third", title: "About" },
-        { key: "fourth", title: "Contact" },
-    ]);
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "Home" },
+    { key: "second", title: "Study" },
+    { key: "third", title: "About" },
+    { key: "fourth", title: "Contact" },
+  ]);
 
-    return (
-        <View
-            style={{
-                flex: 1,
-                paddingTop:
-                    Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
-            }}
-        >
-            <View style={styles.header}>
-                <Image
-                    source={logo}
-                    style={{ width: 50, height: 50, marginLeft: 10 }}
-                />
-                <Text style={styles.headerText}>Your Study App</Text>
-                <Icon
-                    name="user-circle"
-                    type="font-awesome"
-                    color="#FFF"
-                    size={28}
-                    iconStyle={{ marginRight: 10 }}
-                />
-            </View>
-            <TabView
-                navigationState={{ index, routes }}
-                renderScene={renderScene}
-                onIndexChange={setIndex}
-                initialLayout={{ width: layout.width }}
-                renderTabBar={(props) => (
-                    <TabBar
-                        {...props}
-                        indicatorStyle={{
-                            backgroundColor: "#FFFF00",
-                        }}
-                        style={{ backgroundColor: "#008080" }}
-                    />
-                )}
-            />
-        </View>
-    );
-}
-// "#008080" #09B4B7 #06466B #E5E8E7 #B3B5B7 #010114
-const styles = StyleSheet.create({
-    container: {
+  return (
+    <View
+      style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    header: {
-        height: 60,
-        backgroundColor: "#0ABAB5",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
-    headerText: {
-        fontSize: 22,
-        fontWeight: "bold",
-        fontColor: "#010114",
-    },
+        paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+      }}
+    >
+      <View style={styles.header}>
+        <Image
+          source={logo}
+          style={{ width: 50, height: 50, marginLeft: 10 }}
+        />
+        <Text style={styles.headerText}>Your Study App</Text>
+        <Icon
+          name="user-circle"
+          type="font-awesome"
+          color="#FFF"
+          size={28}
+          iconStyle={{ marginRight: 10 }}
+        />
+      </View>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            indicatorStyle={{
+              backgroundColor: "#FFFF00",
+            }}
+            style={{ backgroundColor: "#008080" }}
+          />
+        )}
+      />
+    </View>
+  );
+}
+// "#008080" #09B4B7 #06466B #E5E8E7 #B3B5B7 #010114 #FFFF00
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    height: 60,
+    backgroundColor: "#0ABAB5",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    fontColor: "#010114",
+  },
 });
